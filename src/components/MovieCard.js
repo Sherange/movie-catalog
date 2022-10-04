6af688248e1f56f67b4c6d236acaf1e243a8d7a5
Sherange <sherangef@gmail.com>
2022-10-04 23:52:48 +0530

    add tab view to main screen

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {AirbnbRating} from '@rneui/themed';
 import {primaryTextColor, secondryTextColor} from '../constants/theme';
 
 const MovieCard = ({item}) => {
   return (
     <TouchableOpacity style={styles.movieCardStyle} onPress={() => {}}>
       <Image style={styles.movieCardThumbnail} source={{uri: item.image}} />
+      <AirbnbRating
+        count={5}
+        defaultRating={parseInt(item.imDbRating, 10) / 2}
+        isDisabled={true}
+        reviews={[]}
+        showRating={false}
+        size={14}
+        ratingContainerStyle={styles.ratingContainerStyle}
+      />
       <Text style={styles.movieTitleStyle}>{item.title}</Text>
       <Text style={styles.movieSubtitleStyle}>
         {item.year} | {item.imDbRating}
@@ -31,6 +41,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'flex-start',
     marginTop: 27,
+    marginLeft: 16,
   },
   movieCardThumbnail: {
     width: 164,
@@ -38,13 +49,19 @@ const styles = StyleSheet.create({
   },
   movieTitleStyle: {
     fontSize: 16,
+    fontWeight: '600',
     color: primaryTextColor,
     marginTop: 7,
   },
   movieSubtitleStyle: {
     fontSize: 12,
+    fontWeight: '600',
     color: secondryTextColor,
     marginTop: 4,
+  },
+  ratingContainerStyle: {
+    alignItems: 'flex-start',
+    marginTop: 16,
   },
 });
 
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -8,11 +8,18 @@ import {
   useColorScheme,
   StyleSheet,
 } from 'react-native';
+import {Tab, TabView} from '@rneui/themed';
 
 import MovieCard from '../components/MovieCard';
-import {backgroundColor, primaryTextColor} from '../constants/theme';
+import {
+  backgroundColor,
+  primaryTextColor,
+  secondryColor,
+} from '../constants/theme';
 
 const MainScreen = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const items = [
@@ -42,6 +49,64 @@ const MainScreen = () => {
       imDbRating: '6.2',
       imDbRatingCount: '23573',
     },
+    {
+      id: 'tt15474916',
+      rank: '7',
+      rankUpDown: '+29',
+      title: 'Smile',
+      fullTitle: 'Smile (2022)',
+      year: '2022',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BODJkNDY0MDgtYzFmYy00YzMyLThiNGUtOGI3ZjcyNjk0MWFiXkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_Ratio0.6716_AL_.jpg',
+      crew: 'Parker Finn (dir.), Sosie Bacon, Jessie T. Usher',
+      imDbRating: '7.0',
+      imDbRatingCount: '8098',
+    },
+  ];
+
+  const comingSoon = [
+    {
+      id: 'tt10751150',
+      title: 'Pardoned by Grace',
+      fullTitle: 'Pardoned by Grace (2022)',
+      year: '2022',
+      releaseState: '04 October 2022',
+      image: 'https://imdb-api.com/images/128x176/nopicture.jpg',
+      runtimeMins: null,
+      runtimeStr: null,
+      plot: null,
+      contentRating: null,
+      imDbRating: null,
+      imDbRatingCount: null,
+      metacriticRating: null,
+      genres: null,
+      genreList: [],
+      directors: null,
+      directorList: [],
+      stars: null,
+      starList: [],
+    },
+    {
+      id: 'tt19770238',
+      title: 'Aftersun',
+      fullTitle: 'Aftersun (2022)',
+      year: '2022',
+      releaseState: '05 October 2022',
+      image: 'https://imdb-api.com/images/128x176/nopicture.jpg',
+      runtimeMins: null,
+      runtimeStr: null,
+      plot: null,
+      contentRating: null,
+      imDbRating: null,
+      imDbRatingCount: null,
+      metacriticRating: null,
+      genres: null,
+      genreList: [],
+      directors: null,
+      directorList: [],
+      stars: null,
+      starList: [],
+    },
   ];
 
   const renderItem = ({item}) => {
@@ -55,14 +120,39 @@ const MainScreen = () => {
         backgroundColor={styles.backgroundStyle.backgroundColor}
       />
       <View style={styles.mainContainer}>
-        <Text style={styles.titeStyle}>Main Scren</Text>
-        <FlatList
-          data={items}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          numColumns={2}
-          // extraData={selectedId}
-        />
+        <Tab
+          value={tabIndex}
+          onChange={tab => setTabIndex(tab)}
+          variant="primary"
+          indicatorStyle={{backgroundColor: secondryColor}}>
+          <Tab.Item containerStyle={{backgroundColor: backgroundColor}}>
+            <Text style={styles.titeStyle}>Most Popular Movies</Text>
+          </Tab.Item>
+          <Tab.Item containerStyle={{backgroundColor: backgroundColor}}>
+            <Text style={styles.titeStyle}>Coming Soon</Text>
+          </Tab.Item>
+        </Tab>
+
+        <TabView value={tabIndex} onChange={setTabIndex}>
+          <TabView.Item style={styles.tabViewContainerStyle}>
+            <FlatList
+              data={items}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+              numColumns={2}
+              // extraData={selectedId}
+            />
+          </TabView.Item>
+          <TabView.Item style={styles.tabViewContainerStyle}>
+            <FlatList
+              data={comingSoon}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+              numColumns={2}
+              // extraData={selectedId}
+            />
+          </TabView.Item>
+        </TabView>
       </View>
     </SafeAreaView>
   );
@@ -86,6 +176,10 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     textAlign: 'left',
     color: primaryTextColor,
+    marginVertical: 8,
+  },
+  tabViewContainerStyle: {
+    flex: 1,
   },
 });
 
